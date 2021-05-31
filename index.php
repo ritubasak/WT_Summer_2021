43d97b144fee7a0ec687532cc676c27f4fe7d6ad
Your Name <you@example.com>
2021-05-31 18:47:28 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+   <meta charset="UTF-8">
+   <title>My First Page</title>
+   </head>
+<body>
+<main><p>This is a paraghraph</p></main>
+<main><h2><p>This is a paraghraph</p></h2></main>
+</body>
+</html>
