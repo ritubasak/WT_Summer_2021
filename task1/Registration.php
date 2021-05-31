9af156db9b82ad05097a0c39d74ddec0532cc297
ritubasak <84726511+ritubasak@users.noreply.github.com>
2021-05-31 19:15:44 +0600

    Add files via upload

---
+++ task1/Registration.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Task1</title>
+  </head>
+  <body>
+  <form action="#" method="POST">
+    <fieldset>
+      <legend><h2>Registration Form</h2></legend>
+    
+    <table border="0">
+
+      <tr>
+       
+        <td>Full Name:</td>
+		<td></td>
+		<td></td>
+        <td><input name="User name" type="text"></td>		
+      </tr>
+
+      <tr>
+       
+        <td>E-mail:</td>
+		<td></td>
+		<td></td>
+        <td><input name="E-mail" type="text"></td>
+
+      </tr>
+	  
+	   <tr>
+        
+        <td>Password:</td>
+		<td></td>
+		<td></td>
+        <td><input name="pass" type="password"></td>
+
+      </tr>
+	  
+	  <tr>
+       
+        <td>Comment:</td>
+		<td></td>
+		<td></td>
+        <td><textarea name="Basak" id="5" cols="40" rows="6">Enter your comment here!</textarea></td>
+
+      </tr>
+	  <tr>
+        
+        <td>Gender:</td>
+		<td></td>
+		<td></td>
+		<td>
+        <input type="radio" name="gender">Female
+		<input type="radio" name="gender">Male
+		<input type="radio" name="gender">Other
+		</td>
+
+      </tr>
+	  
+	   <tr>
+        
+        <td>Please Choose a hobby:</td>
+		<td></td>
+		<td></td>
+        <td>
+		<input type="checkbox">Singing
+		<input type="checkbox">Danching
+		<input type="checkbox">Reading
+		</td>
+
+      </tr>
+	  
+	  <tr>
+       
+        <td>Please Choose a file:</td>
+		<td></td>
+		<td></td>
+        <td> <input type="file"</td>
+      </tr>
+
+    </table>
+	
+	  <input type="submit" value="Submit">
+	  <input type="reset">
+	  
+	</fieldset>
+  </form>
+  </body>
+</html>
